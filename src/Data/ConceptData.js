6246495b2b4c585ace3a21a51c56f762daf7deb8
Repo <art@beardigitalstudios.com>
2024-02-19 13:39:24 +0000

    separated the data so that I can add more questions without messing up the main file

---
+++ src/Data/ConceptData.js
@@ -0,0 +1,25 @@
+const conceptsData = [
+  {
+    id: 1,
+    name: "Variables",
+    description:
+      "Variables are used to store data that can be referenced and manipulated in a program.",
+    // image: "/variable.jpg",
+    quizQuestion: "What is a variable?",
+    quizOptions: ["A value that can change", "A constant value", "A function"],
+    quizAnswer: "A value that can change",
+  },
+  {
+    id: 2,
+    name: "Functions",
+    description:
+      "Functions are blocks of reusable code that perform a specific task.",
+    // image: "/function.jpg",
+    quizQuestion: "What are functions?",
+    quizOptions: ["Variables", "Loops", "Blocks of reusable code"],
+    quizAnswer: "Blocks of reusable code",
+  },
+  // Add more concepts as needed
+];
+
+export default conceptsData;
--- src/app/page.js
+++ src/app/page.js
@@ -2,30 +2,7 @@
 import { useState } from "react";
 import ConceptList from "../components/ConceptList";
 import ConceptDetails from "../components/ConceptDetails";
-
-const conceptsData = [
-  {
-    id: 1,
-    name: "Variables",
-    description:
-      "Variables are used to store data that can be referenced and manipulated in a program.",
-    // image: "/variable.jpg",
-    quizQuestion: "What is a variable?",
-    quizOptions: ["A value that can change", "A constant value", "A function"],
-    quizAnswer: "A value that can change",
-  },
-  {
-    id: 2,
-    name: "Functions",
-    description:
-      "Functions are blocks of reusable code that perform a specific task.",
-    // image: "/function.jpg",
-    quizQuestion: "What are functions?",
-    quizOptions: ["Variables", "Loops", "Blocks of reusable code"],
-    quizAnswer: "Blocks of reusable code",
-  },
-  // Add more concepts as needed
-];
+import conceptsData from "../Data/ConceptData"; // Corrected import statement
 
 export default function Home() {
   const [selectedConcept, setSelectedConcept] = useState(conceptsData[0]);
